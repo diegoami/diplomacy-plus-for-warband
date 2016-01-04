8e1f64b6b92c93a5e911252426a2fca68800b400
Diego Amicabile <diego.amicabile@gmail.com>
2016-01-04 13:58:31 +0100

    Implemented bodyguard

--- Module_system 1.165/module_dialogs.py
+++ Module_system 1.165/module_dialogs.py
@@ -3460,6 +3460,7 @@ Please, take this as some small repayment for your noble deed.", "rescue_prisone
 
 [anyone, "start", [(is_between, "$g_talk_troop", companions_begin, companions_end),
                (this_or_next|eq, "$talk_context", tc_tavern_talk),
+                  (this_or_next|eq, "$talk_context", tc_town_talk), #ADD THIS LINE FOR BODYGUARD CODE
             (eq, "$talk_context", tc_court_talk),
                (main_party_has_troop, "$g_talk_troop")],
 "Let's leave whenever you are ready.", "close_window", []],
